b8c9d79d961417ac7896ba8dd1bdd5ebe00fbf8a
Mauricio Juárez <iconoclastsym@gmail.com>
2023-11-13 17:55:52 -0600

    fixed request code

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 app.post('/webhook', async (req, res) => {
-    // Extract the file from the request
-    // This depends on how the file is sent in the request
     const filePath = '1_updated_ConstanciaFiscalOECG.pdf';
-
     const projectId = 'documentprincipal';
     const location = 'us';
     const processorId = '69de61eeb827ed15';
@@ -26,22 +23,13 @@ app.post('/webhook', async (req, res) => {
     const document = await processDocument(projectId, location, processorId, filePath);
     console.log(document);
 
-    // Send a response back to Dialogflow
-    // You might want to format this response so that it can be displayed nicely in Facebook Messenger
     res.json({ fulfillmentText: 'Document processed successfully!', document: document });
 });
 
 async function processDocument(projectId, location, processorId, filePath) {
-    // Instantiates a client
     const documentaiClient = new DocumentProcessorServiceClient();
-
-    // The full resource name of the processor
     const resourceName = documentaiClient.processorPath(projectId, location, processorId);
-
-    // Read the file into memory.
     const imageFile = fs.readFileSync(filePath);
-
-    // Determine the MIME type based on the file extension
     const extension = filePath.split('.').pop();
     let mimeType;
     switch (extension) {
@@ -61,22 +49,15 @@ async function processDocument(projectId, location, processorId, filePath) {
         default:
             throw new Error(`Unsupported file extension: ${extension}`);
     }
-
-    // Load Binary Data into Document AI RawDocument Object
     const rawDocument = {
         content: imageFile,
         mimeType: mimeType,
     };
-
-    // Configure ProcessRequest Object
     const request = {
         name: resourceName,
         rawDocument: rawDocument
     };
-
-    // Use the Document AI client to process the sample form
     const [result] = await documentaiClient.processDocument(request);
-
     return result.document;
 }
 
