b8d0a4820c9bf785b878fe2bd9b35dbde0539d12
Mauricio Juárez <iconoclastsym@gmail.com>
2023-11-13 15:39:27 -0600

    converting to custom extractor

--- index.js
+++ index.js
@@ -1,68 +1,40 @@
 const { DocumentProcessorServiceClient } = require('@google-cloud/documentai').v1;
 const fs = require('fs');
 
-/**
- * Extract form data and confidence from processed document.
- */
-function extractFormData(document) {
-    // Extract shards from the text field
-    function getText(textAnchor, document) {
-        if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
-            return '';
-        }
-
-        // First shard in document doesn't have startIndex property
-        const startIndex = textAnchor.textSegments[0].startIndex || 0;
-        const endIndex = textAnchor.textSegments[0].endIndex;
-
-        return document.text.substring(startIndex, endIndex);
-    }
-
-    var formData = [];
-
-    const pages = document.pages;
-
-    pages.forEach((page) => {
-        const formFields = page.formFields;
-        formFields.forEach((field) => {
-            // Get the extracted field names and remove extra space from text
-            const fieldName = getText(field.fieldName.textAnchor, document);
-            // Confidence - How "sure" the API is that the text is correct
-            const nameConfidence = field.fieldName.confidence.toFixed(4);
-
-            const fieldValue = getText(field.fieldValue.textAnchor, document);
-            const valueConfidence = field.fieldValue.confidence.toFixed(4);
-
-            formData.push({
-                fieldName: fieldName,
-                fieldValue: fieldValue,
-                nameConfidence: nameConfidence,
-                valueConfidence: valueConfidence
-            });
-        });
-    });
-
-    return formData;
-}
-
-async function processDocument(projectId, location, processorId, filePath, mimeType) {
+async function processDocument(projectId, location, processorId, filePath) {
     // Instantiates a client
     const documentaiClient = new DocumentProcessorServiceClient();
 
-    // The full resource name of the processor, e.g.:
-    // projects/project-id/locations/location/processor/processor-id
-    // You must create new processors in the Cloud Console first
+    // The full resource name of the processor
     const resourceName = documentaiClient.processorPath(projectId, location, processorId);
 
     // Read the file into memory.
     const imageFile = fs.readFileSync(filePath);
 
-    // Convert the image data to a Buffer and base64 encode it.
-    const encodedImage = Buffer.from(imageFile).toString('base64');
+    // Determine the MIME type based on the file extension
+    const extension = filePath.split('.').pop();
+    let mimeType;
+    switch (extension) {
+        case 'pdf':
+            mimeType = 'application/pdf';
+            break;
+        case 'png':
+            mimeType = 'image/png';
+            break;
+        case 'jpg':
+        case 'jpeg':
+            mimeType = 'image/jpeg';
+            break;
+        case 'tiff':
+            mimeType = 'image/tiff';
+            break;
+        default:
+            throw new Error(`Unsupported file extension: ${extension}`);
+    }
 
     // Load Binary Data into Document AI RawDocument Object
     const rawDocument = {
-        content: encodedImage,
+        content: imageFile,
         mimeType: mimeType,
     };
 
@@ -78,27 +50,17 @@ async function processDocument(projectId, location, processorId, filePath, mimeT
     return result.document;
 }
 
-/**
- * Run the codelab.
- */
 async function main() {
     const projectId = 'documentprincipal';
     const location = 'us'; // Format is 'us' or 'eu'
-    const processorId = '3f7983dcf3c6d6c6'; // Should be a Hexadecimal string
+    const processorId = '69de61eeb827ed15'; // Should be a Hexadecimal string
+    const filePath = '1_updated_ConstanciaFiscalOECG.pdf'; // The local file in your current working directory
 
-    // Supported File Types
-    // https://cloud.google.com/document-ai/docs/processors-list#processor_form-parser
-    filePath = 'ConstanciaFiscalOECG.pdf'; // The local file in your current working directory
-    mimeType = 'application/pdf';
-
-    const document = await processDocument(projectId, location, processorId, filePath, mimeType);
-    const formData = extractFormData(document);
-
-    console.log('\nThe following form key/value pairs were detected:');
-    console.log(formData);
+    const document = await processDocument(projectId, location, processorId, filePath);
+    console.log(document);
 }
 
-main(...process.argv.slice(2)).catch(err => {
+main().catch(err => {
     console.error(err);
     process.exitCode = 1;
 });
